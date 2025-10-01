12ca5b0c746c19dfa7fb3a5296cfba94859b4ec8
DmitriyKalashnikov1 <slagterra2017@yandex.ru>
2025-10-01 09:23:51 +0300

    gpu clustering test failed

--- AnalyzeService/testClusteriztion.py
+++ AnalyzeService/testClusteriztion.py
@@ -4,19 +4,22 @@ from tqdm import tqdm
 tqdm.pandas()
 from pandarallel import pandarallel
 from textPreprocessing import textToLemmas
-from textClusterisation import clusterizeTextToBagOfWords, getThemes
+from textClusterisation import Clusterizator
 from time import time
 
+tC = Clusterizator(pathToClusterizer='../data/gpuClusterizator', pathToVectorizer="../data/tfidfVectorizer.joblib")
+
 def textClustering(text:str, vectorOrName: bool):
+    global tC
     # split text by part
     parts = re.split(r'[.!?:;]+', text)
     lemmParts = [textToLemmas(part) for part in parts]
 
     if (vectorOrName):
-        return clusterizeTextToBagOfWords(lemmParts,'../data/mlClusterizatorLemmText.joblib')[0]
+        return tC.clusterizeTextToBagOfWords(lemmParts)[0]
     else:
-        bow = clusterizeTextToBagOfWords(lemmParts,'../data/mlClusterizatorLemmText.joblib')
-        return getThemes(bow)
+        bow = tC.clusterizeTextToBagOfWords(lemmParts)
+        return tC.getThemes(bow)
 
 pandarallel.initialize(nb_workers=1, use_memory_fs=True, progress_bar=True)
 
@@ -31,7 +34,7 @@ print(textClustering(texts[32], False))
 print(f"time of one clusterisation: {(time() - t0)/2} s")
 
 print("Adding information to dataset...")
-df['clusterVector'] = df["text"].parallel_apply(lambda x: textClustering(x, True))
+df['clusterVector'] = df["text"].apply(lambda x: textClustering(x, True))
 #df['textThemes'] = df['text'].apply(lambda x: textClustering(x, False))
 df.to_csv("../data/dataset.csv")
 print("Done")
--- AnalyzeService/textClusterisation.py
+++ AnalyzeService/textClusterisation.py
@@ -1,44 +1,46 @@
 import numpy as np
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import make_pipeline
-from sklearn.cluster import KMeans
-from sklearn.decomposition import TruncatedSVD
-from sklearn.preprocessing import Normalizer
+from hummingbird.ml import load
 
-categoriesNames = ["Привилегии и сервис", "Карты, переводы и операции", "Акции и бонусы",
+class Clusterizator():
+    categoriesNames = ["Привилегии и сервис", "Карты, переводы и операции", "Акции и бонусы",
                    "Поддержка и связь", "Доставка и встречи", "Качество обслуживания",
                    "Счета, вклады и закрытие", "Коммуникация и документы", "Прочие",
                    "Пусто"]
-categoriesClusters = [[1,10,32], [2,4,9,12,17,21,22,30], [3,20,31,34], [5,15,28,33],
+    categoriesClusters = [[1,10,32], [2,4,9,12,17,21,22,30], [3,20,31,34], [5,15,28,33],
                       [16,25], [6,7,8,26,27], [18,19,29], [13,23], [11,24], [14]]
+    idfVectorizer = None
+    mlClusterizator = None
 
+    def __init__(self, pathToClusterizer:str, pathToVectorizer:str):
+        self.idfVectorizer = joblib.load(pathToVectorizer)
+        self.mlClusterizator = load(pathToClusterizer, override_flag=True)
+        self.mlClusterizator = self.mlClusterizator.to("cuda")
 
-def clusterizeTextToBagOfWords(listsOfTexts: list, pathToClusterizer:str):
-    outVectors = []
+    def clusterizeTextToBagOfWords(self, listsOfTexts: list):
+        outVectors = []
 
-    mlClusterizator = joblib.load(pathToClusterizer)
+        clusters = self.mlClusterizator.predict(self.idfVectorizer.transform(listsOfTexts).todense().A)
 
-    clusters = mlClusterizator.predict(listsOfTexts)
-    del mlClusterizator
-    for f in range(len(listsOfTexts)):
-        bow = np.zeros(shape=(1, len(categoriesClusters)))
-        cluster = clusters[f]
+        for f in range(len(listsOfTexts)):
+            bow = np.zeros(shape=(1, len(self.categoriesClusters)))
+            cluster = clusters[f]
 
-        for index, list in enumerate(categoriesClusters):
-            if cluster in list:
-                bow[0,index] = 1
-        outVectors.append(bow)
+            for index, list in enumerate(self.categoriesClusters):
+                if cluster in list:
+                    bow[0,index] = 1
+            outVectors.append(bow)
 
-    return np.any(np.array(outVectors), axis=0).astype(np.integer)
+        return np.any(np.array(outVectors), axis=0).astype(np.integer)
 
-def getThemes(bow: np.array):
-    output = []
-    for f in range(bow.shape[0]):
-        for index, c in enumerate(bow[f]):
-            if c:
-                output.append(categoriesNames[index])
-        return output
+    def getThemes(self, bow: np.array):
+        output = []
+        for f in range(bow.shape[0]):
+            for index, c in enumerate(bow[f]):
+                if c:
+                    output.append(self.categoriesNames[index])
+            return output
 
 
 
